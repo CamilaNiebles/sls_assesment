b27d6c4125d6a690f20a93c27afdc97844dc25d3
María Camila Niebles Reyes <niebles.reyes@gmail.com>
2026-01-25 19:28:46 -0500

    feat: Health check endpoint added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+console.log("Starting app.ts");
 import express, { type Express } from "express";
+import { healthRouter } from "./controllers/health.router";
 
 const app : Express = express();
 
+app.use((req, _res, next) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
+
 app.use(express.json());
+
+
+app.use('/health-check', healthRouter);
 
 export default app;
